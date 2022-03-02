6ae5def6c53d55f12e641248b8f41ab1843fd10f
Alexandra Salén Sammueang <alexandra.salensammueang@edu.newton.se>
2022-03-02 14:56:49 +0100

    Comment out in program

--- PearlAssignment/Program.cs
+++ PearlAssignment/Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using PearlAssignment;
 
-var pearl = Pearl.Factory.CreateWithRandomData();
+//var pearl = Pearl.Factory.CreateWithRandomData();
 
-Console.WriteLine(pearl);
+//Console.WriteLine(pearl);
 
 INecklace necklace = Necklace.Factory.CreateWithRandomData(35);
 Console.WriteLine($"Our necklace contains 35 different pearls: \n{necklace}");
