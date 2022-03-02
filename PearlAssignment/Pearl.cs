32c80afe80482f2dc9d1270762b7e5db83b34e6f
Alexandra Salén Sammueang <alexandra.salensammueang@edu.newton.se>
2022-03-02 12:03:27 +0100

    Pearl.

--- PearlAssignment/Pearl.cs
+++ PearlAssignment/Pearl.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PearlAssignment
+﻿namespace PearlAssignment
 {
-    internal class Pearl
+    class Pearl
     {
+       
+
+        public void RandomInit()
+        {
+       
+
+        var rnd = new Random();
+        bool bAllOK = false;
+            while (!bAllOK)
+            {
+                try
+                {
+                    int size = rnd.Next(5,26);
+                    //color
+                    //Form
+                    //Type enum
+
+
+                  
+                    //            this.Type = (Type) rnd.Next((int) Type.Sweetwater, (int) Type.Saltwather + 1);
+
+                    //string[] _firstnames = "Fred John Mary Jane Oliver Marie".Split(' ');
+                    //string[] _lastnames = "Johnsson Pearsson Smith Ewans Andersson".Split(' ');
+                    //            this.FirstName = _firstnames[rnd.Next(0, _firstnames.Length)];
+                    //            this.LastName = _lastnames[rnd.Next(0, _lastnames.Length)];
+
+                    bAllOK = true;
+                }
+                catch { }
+            }
+        }
+
     }
 }
