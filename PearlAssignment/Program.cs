ddf0a0ef4a65d3c9c6912a4a53f37be9b267e88f
Alexandra Salén Sammueang <alexandra.salensammueang@edu.newton.se>
2022-03-02 13:50:10 +0100

    change in program

--- PearlAssignment/Program.cs
+++ PearlAssignment/Program.cs
@@ -6,4 +6,4 @@ var pearl = Pearl.Factory.CreateWithRandomData();
 Console.WriteLine(pearl);
 
 INecklace necklace = Necklace.Factory.CreateWithRandomData(35);
-Console.WriteLine($"We have: {necklace} pearls");
+Console.WriteLine($"Our necklace contains 35 different pearls: \n{necklace}");
